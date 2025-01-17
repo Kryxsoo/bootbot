c1d0f533629a913285929bb7d06935a4fe8217b6
Mathew Charm <mattcharmz@gmail.com>
2025-01-17 08:27:09 -0800

    added char counter

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ def main():
 
   booked = book()
   counted = counter(booked)
-  print(counted)
+  chars_dict = get_character(booked)
+  print(chars_dict)
+
+def char(booked):
+  match = []
+  alpha = 0
+  for letter in booked:
+    if letter == match:
+      alpha += 1
+    return alpha
 
 def counter(book):
   counted = len(book.split())
   return counted
+
+def get_character(book):
+  chars = {}
+  for c in book:
+    lowered = c.lower()
+    if lowered in chars:
+      chars[lowered] += 1
+    else:
+      chars[lowered] = 1
+  return chars
 
 def book():
   with open("books/frankenstein.txt") as f:
